d7cf92e0b5b20159600f154f3f7b31911e0cf9aa
gofranaldaraa <99589023+gofranaldaraa@users.noreply.github.com>
2022-02-27 11:42:27 +0200

    hell stria

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 class HelloWorld{
 public static void main (String args[]){
     System.out.println("Hello Noara!");
-    System.out.println("Hell Gofra!");
+    System.out.println("Hell Gofran!");
+     System.out.println("Hell Syria!");
 }
 
 
